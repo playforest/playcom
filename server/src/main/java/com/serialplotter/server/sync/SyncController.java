09b55a98f90cac478e5c444fdbf79b0d6f7bff82
Imran Dhahbi <playforest0x@gmail.com>
2023-04-25 16:25:48 +1000

    fix method mappings and repository custom query

--- server/src/main/java/com/serialplotter/server/sync/SyncController.java
+++ server/src/main/java/com/serialplotter/server/sync/SyncController.java
@@ -1,2 +1,33 @@
-package com.serialplotter.server.sync;public class SyncController {
+package com.serialplotter.server.sync;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="api/v1/sync")
+public class SyncController {
+
+    private final SyncService syncService;
+
+    @Autowired
+    public SyncController(SyncService syncService) {
+        this.syncService = syncService;
+    }
+
+    @GetMapping(path="/{userId}/{streamId}")
+    public String getData(@PathVariable Long userId, @PathVariable Long streamId) {
+        return syncService.getData(userId, streamId);
+    }
+
+    @PostMapping(path="/{userId}/{streamId}")
+    public void  updateData(@PathVariable Long userId, @PathVariable Long streamId) {
+
+    }
+
+    @DeleteMapping(path="/{userId}/{streamId}")
+    public void deleteData(@PathVariable Long userId, @PathVariable Long streamId) {
+
+    }
+
 }
--- server/src/main/java/com/serialplotter/server/sync/SyncRepository.java
+++ server/src/main/java/com/serialplotter/server/sync/SyncRepository.java
@@ -1,2 +1,9 @@
-package com.serialplotter.server.sync;public interface SyncRepository {
+package com.serialplotter.server.sync;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SyncRepository extends JpaRepository<Sync, Long> {
+    List<Sync> findSyncJobsByUserId(Long userId);
 }
