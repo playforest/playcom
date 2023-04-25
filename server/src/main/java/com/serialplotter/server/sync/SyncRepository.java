331ad0ddcd509b9ff37153ce5ffc5d6f372f8f92
Imran Dhahbi <playforest0x@gmail.com>
2023-04-25 20:50:35 +1000

    custom query to get latest sync job

--- server/src/main/java/com/serialplotter/server/sync/SyncRepository.java
+++ server/src/main/java/com/serialplotter/server/sync/SyncRepository.java
@@ -1,9 +1,19 @@
 package com.serialplotter.server.sync;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface SyncRepository extends JpaRepository<Sync, Long> {
     List<Sync> findSyncJobsByUserId(Long userId);
+    Sync findFirstByStreamIdAndStatusOrderBySyncIdDesc(Long streamId, String status);
+
+    @Query(value = """
+                    SELECT s FROM Sync s 
+                    WHERE s.status = :status AND s.streamId = :streamId 
+                    ORDER BY s.syncId DESC LIMIT 1
+                    """)
+    Sync findLatestSuccessfulSyncJob(Long streamId, String status);
 }
