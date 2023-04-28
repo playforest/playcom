69c5104002f9ef55e77feb4ee404149465939465
Imran Dhahbi <playforest0x@gmail.com>
2023-04-28 21:23:38 +1000

    feat: user registration: add password encoding service

---
+++ server/src/main/java/com/serialplotter/server/security/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.serialplotter.server.security.config;
+
+import com.serialplotter.server.user.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public WebSecurityConfig(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManagerBuilder auth) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests()
+                .requestMatchers("/api/v*/registration/**")
+                .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin();
+
+        auth.authenticationProvider(daoAuthenticationProvider());
+
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+}
