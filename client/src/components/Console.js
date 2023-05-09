414da68c949ad6fdaa7b249f8735e191c4bd960b
Imran Dhahbi <playforest0x@gmail.com>
2023-05-10 02:10:32 +1000

    UI: Add icons to card titles

--- client/src/components/Console.js
+++ client/src/components/Console.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Card } from 'antd';
-import ErrorBoundary from 'antd/es/alert/ErrorBoundary';
+import { CodeOutlined } from '@ant-design/icons';
+
+const label = 'Console';
 
 export const Console = () => {
     return (
         <Card 
             size="small" 
-            title="Console" 
+            title={
+                <>
+                  <CodeOutlined /> {label}
+                </>
+            }
             style={{ width: '100%', height: '100%' }}
             bodyStyle={{backgroundColor: '#000'}}
             >
--- client/src/components/Plot.js
+++ client/src/components/Plot.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Card } from 'antd';
+import { SlidersOutlined } from '@ant-design/icons';
+
+const label = 'Plot';
 
 export const Plot = () => {
     return (
-        <Card size="small" title="Plot" style={{ width: '100%' }}>
+        <Card 
+            size="small"
+            title={
+                <>
+                    <SlidersOutlined /> {label}
+                </>
+            } 
+            style={{ width: '100%' }}>
 
         </Card>
     )
