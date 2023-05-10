b5498173c3acab764b0d400384c7151e9bcfa5af
Imran Dhahbi <playforest0x@gmail.com>
2023-05-10 23:54:38 +1000

    client service: connect to serial port

--- client/src/components/Settings/Connect.js
+++ client/src/components/Settings/Connect.js
@@ -1,14 +1,29 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Button, Select, Space } from 'antd';
+import { connect } from '../../services/serial';
 
 export const Connect = () => {
+    const [baudRate, setBaudRate] = useState(9600);
+
+    const handleSelectChange = (value) => {
+        setBaudRate(Number(value));
+    };
+
+    const handleButtonClick = () => {
+        connect(baudRate);
+    };
+
     return (
         <Space direction='vertical'>
-            <Button size='small'>Select Port</Button>
+            <Button 
+                size='small'
+                onClick={handleButtonClick
+                }>Select Port</Button>
             <Select
                 defaultValue="9600"
                 style={{width: 120}}
                 size='small'
+                onChange={handleSelectChange}
                 options={[
                     { value: '9600', label: '9600'},
                     { value: '19200', label: '19200'},
