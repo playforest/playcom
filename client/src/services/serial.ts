fdfcc7e36cd7826caffde212ca0eb6b7d1017b81
Imran Dhahbi <playforest0x@gmail.com>
2023-05-25 20:29:32 +1000

    add: deserialize incoming byte stream from serial port

--- client/src/services/serial.ts
+++ client/src/services/serial.ts
@@ -3,12 +3,20 @@ export class Port {
     private inputStream: ReadableStream<string>;
     private reader: ReadableStreamDefaultReader<string>;
     private baudRate: number;
+    private lineSeperator: string;
+    private line: Uint8Array;
+    private buffer: Uint8Array;
+    private deserialisedData: string[] = [];
 
-    constructor(baudRate: number) {
+    constructor(baudRate: number, lineSeperator: string = '\n') {
         this.port = undefined!;
         this.inputStream = undefined!;
         this.reader = undefined!;
         this.baudRate = baudRate;
+        this.lineSeperator = lineSeperator;
+        this.line = new Uint8Array();
+        this.buffer = new Uint8Array();
+        this.deserialisedData = undefined!;
     }
 
     public async connect() {
@@ -22,6 +30,29 @@ export class Port {
         this.readData();
     }
 
+    public async disconnect() {
+        if (this.reader) {
+            await this.reader.cancel();
+            await this.reader.releaseLock();
+            this.reader = undefined!;
+        }
+
+        if (this.port.readable) {
+            this.port.readable.cancel();
+        }
+
+        if (this.port.writable) {
+            this.port.writable.getWriter().close();
+        }
+
+        await this.port.close().then((result) => {
+            console.log(result);
+        })
+
+        this.port = undefined!;
+    }
+
+
     public async readData() {
         while (this.port.readable) {
             const reader = this.port.readable.getReader();
@@ -34,7 +65,8 @@ export class Port {
                         break;
                     }
                     // Do something with |value|…
-                    console.log('value: ', value);
+                    // console.log('value: ', value);
+                    this.storeDeserialisedData(value);
                 }
             } catch (error) {
                 // Handle |error|…
@@ -42,6 +74,52 @@ export class Port {
                 reader.releaseLock();
             }
         }
+    }
+
+    private storeDeserialisedData(chunk: Uint8Array) {
+        const asciiDelimiter: number = this.lineSeperator.charCodeAt(0);
+        const delimiterIndex = chunk.indexOf(asciiDelimiter);
+        const chunkContainsDelimiter: boolean = delimiterIndex === -1 ? false : true;
+        // console.log('chunkContainsDelimiter: ', chunkContainsDelimiter)
+
+
+        if (!chunkContainsDelimiter) {
+            this.buffer = new Uint8Array(this.line.length + chunk.length);
+            this.buffer.set(this.line);
+            this.buffer.set(chunk, this.line.length);
+
+            this.line = new Uint8Array(this.buffer.length);
+            this.line.set(this.buffer);
+
+            this.buffer = new Uint8Array(0);
+        } else {
+            // construct new buffer with the line up to (and including) the delimiter
+            this.buffer = new Uint8Array(this.line.length + delimiterIndex + 1);
+            this.buffer.set(this.line);
+            this.buffer.set(chunk.slice(0, delimiterIndex + 1), this.line.length);
+
+            // update the line with the remainder of the chunk
+            this.line = chunk.slice(delimiterIndex + 1);
+
+            // decode buffer into a string
+            const decoder = new TextDecoder();
+            const string = decoder.decode(this.buffer);
+            console.log(string)
+            this.deserialisedData.push(string);
+        }
+    }
+
+    public getPortState() {
+        if (this.port) {
+            return {
+                port: this.port.getInfo(),
+                baudRate: this.baudRate,
+                lineSeperator: this.lineSeperator,
+                data: this.buffer,
+                info: this.port.getInfo()
+            };
+        }
+        return null;
     }
 }
 
