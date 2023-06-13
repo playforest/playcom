07933fb4e67411f5c54713c9027ef46ca9d16dee
Imran Dhahbi <playforest0x@gmail.com>
2023-06-13 16:52:46 +1000

    add: console: window-scrolling

--- client/src/components/Console.js
+++ client/src/components/Console.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { Card } from 'antd';
+
+
+import React, { useRef, useEffect } from 'react';
+import { VariableSizeList as List } from 'react-window';
+import { useSelector } from 'react-redux';
+import { Card, Row } from 'antd';
 import { CodeOutlined } from '@ant-design/icons';
 
 const label = 'Console';
 
 export const Console = () => {
+    const data = useSelector((state) => state.serialPort.data);
+    const listRef = useRef(null);
+
+    const getItemSize = (index) => {
+        return 12;
+    }
+
+    useEffect(() => {
+        listRef.current?.scrollToItem(data.length - 1);
+    }, [data]);
+
+    const Row = ({ index, style, data }) => (
+        <div style={style}>
+            <p
+                style={{ color: 'white', fontFamily: 'menlo', fontSize: '10px', lineHeight: '7px' }}>
+                {data[index].data}
+            </p>
+        </div>
+    )
+
     return (
-        <Card 
-            size="small" 
+        <Card
+            size="small"
             title={
                 <>
-                  <CodeOutlined /> {label}
+                    <CodeOutlined /> {label}
                 </>
             }
             style={{ width: '100%', height: '100%' }}
-            bodyStyle={{backgroundColor: '#000'}}
+            bodyStyle={{ backgroundColor: '#000' }}
+        >
+
+            <List
+                ref={listRef}
+                height={200}
+                itemCount={data.length}
+                itemSize={getItemSize}
+                width={'100%'}
+                itemData={data}
             >
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
-        </Card>
+                {Row}
+            </List>
+
+        </Card >
     )
 }
+
